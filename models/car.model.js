75d815913929270b9ae82334da548d423dc5285d
Pedro Henrique <pedrohgbarbosa@hotmail.com>
2023-02-23 21:22:31 -0300

    semi-done

--- models/car.model.js
+++ models/car.model.js
@@ -3,9 +3,10 @@ import { Schema, model } from "mongoose";
 const carSchema = new Schema({
   name: { type: String, required: true },
   brand: { type: String, required: true },
-  year: { type: Date, required: true },
+  year: { type: Number },
   color: { type: String },
   doors: { type: Number },
+  motorization: { type: Number },
 });
 
 export const CarModel = model("Car", carSchema);
--- routes/car.routes.js
+++ routes/car.routes.js
@@ -3,10 +3,65 @@ import { CarModel } from "../models/car.model.js";
 
 const carRouter = express.Router();
 
+//create//body-json-post
 carRouter.post("/", async (req, res) => {
   try {
+    if (req.headers.authorization) {
+      return res.status(401).json("Não autorizado.");
+    }
     const newCar = await CarModel.create({ ...req.body });
+
     return res.status(201).json(newCar);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//get all- get
+carRouter.get("/", async (req, res) => {
+  try {
+    const cars = await CarModel.find({});
+    return res.status(201).json(cars);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//find - get
+carRouter.get("/:carId", async (req, res) => {
+  try {
+    const { carId } = req.params;
+
+    const car = await CarModel.findOne({ _id: carId });
+    return res.status(200).json(car);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//edit - body, json
+//put: editar muitas informações
+//path: editar informações pontuais
+carRouter.put("/:carId", async (req, res) => {
+  try {
+    const { carId } = req.params;
+    const updateCar = await CarModel.findOneAndUpdate(
+      { _id: carId },
+      { ...req.body },
+      { new: true, runValidators: true }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//delete - del
+carRouter.delete("/:carId", async (req, res) => {
+  try {
+    const { carId } = req.params;
+    const del = await CarModel.deleteOne({ _id: carId });
+
+    return res.status(201).json(del);
   } catch (err) {
     console.log(err);
   }
